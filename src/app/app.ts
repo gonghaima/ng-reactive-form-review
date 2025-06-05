abfa7475efb3d7b3dccfe0102bb64df99bcfb854
Steven Gong <gonghaima@hotmail.com>
2025-06-05 15:14:50 +1000

    reactive form with validator

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NameEditor } from './name-editor/name-editor';
+import { ProfileEditor } from './profile-editor/profile-editor';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NameEditor],
+  imports: [RouterOutlet, NameEditor, ProfileEditor],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected title = 'ng-reactive-form-review';
--- src/app/profile-editor/profile-editor.ts
+++ src/app/profile-editor/profile-editor.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-editor',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './profile-editor.html',
-  styleUrl: './profile-editor.css'
+  styleUrl: './profile-editor.css',
 })
 export class ProfileEditor {
+  profileForm = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+  });
 
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.profileForm.value);
+  }
 }
