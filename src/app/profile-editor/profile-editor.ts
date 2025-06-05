38b56abdf2c8076473c2ee44617bc2d46d12ca95
Steven Gong <gonghaima@hotmail.com>
2025-06-06 08:30:59 +1000

    Group the nested form in the template

--- src/app/profile-editor/profile-editor.ts
+++ src/app/profile-editor/profile-editor.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-profile-editor',
@@ -9,9 +14,26 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 })
 export class ProfileEditor {
   profileForm = new FormGroup({
-    firstName: new FormControl('', Validators.required),
-    lastName: new FormControl('', Validators.required),
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    address: new FormGroup({
+      street: new FormControl(''),
+      city: new FormControl(''),
+      state: new FormControl(''),
+      zip: new FormControl(''),
+    }),
   });
+
+  updateProfile() {
+    this.profileForm.patchValue({
+      firstName: 'Nancy',
+      address: {
+        street: '123 Drew Street',
+      },
+    });
+    console.log(this.profileForm.value);
+    
+  }
 
   onSubmit() {
     // TODO: Use EventEmitter with form value
