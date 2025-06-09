b6769381e086e04cebbe2263ead0f62694219ab5
Steven Gong <gonghaima@hotmail.com>
2025-06-09 18:50:24 +1000

    dynamic form

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,11 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { NameEditor } from './name-editor/name-editor';
 import { ProfileEditor } from './profile-editor/profile-editor';
 import { ProfileEditorV1 } from './profile-editor-v1/profile-editor-v1';
+import { ProfileEditorDynamic } from './profile-editor-dynamic/profile-editor-dynamic';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NameEditor, ProfileEditor, ProfileEditorV1],
+  imports: [
+    RouterOutlet,
+    NameEditor,
+    ProfileEditor,
+    ProfileEditorV1,
+    ProfileEditorDynamic,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
--- src/app/profile-editor-dynamic/profile-editor-dynamic.ts
+++ src/app/profile-editor-dynamic/profile-editor-dynamic.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-profile-editor-dynamic',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './profile-editor-dynamic.html',
-  styleUrl: './profile-editor-dynamic.css'
+  styleUrls: ['../shared/form-styles.css', './profile-editor-dynamic.css'],
 })
 export class ProfileEditorDynamic {
+  private formBuilder = inject(FormBuilder);
+  profileForm = this.formBuilder.group({
+    firstName: ['', Validators.required],
+    lastName: [''],
+    address: this.formBuilder.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      zip: [''],
+    }),
+    aliases: this.formBuilder.array([this.formBuilder.control('')]),
+  });
 
+  get aliases() {
+    return this.profileForm.get('aliases') as FormArray;
+  }
+  addAlias() {
+    this.aliases.push(this.formBuilder.control(''));
+  }
 }
