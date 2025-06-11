9569061eef30dca68e0dc3ddb1e371f9ce10adf4
Steven Gong <gonghaima@hotmail.com>
2025-06-11 10:08:51 +1000

    data driven with formConfig

--- src/app/profile-editor-dynamic/profile-editor-dynamic.ts
+++ src/app/profile-editor-dynamic/profile-editor-dynamic.ts
@@ -7,31 +7,60 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-profile-editor-dynamic',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './profile-editor-dynamic.html',
   styleUrls: ['../shared/form-styles.css', './profile-editor-dynamic.css'],
 })
 export class ProfileEditorDynamic {
   private formBuilder = inject(FormBuilder);
-  profileForm = this.formBuilder.group({
-    firstName: ['', Validators.required],
-    lastName: [''],
-    address: this.formBuilder.group({
-      street: [''],
-      city: [''],
-      state: [''],
-      zip: [''],
-    }),
-    aliases: this.formBuilder.array([this.formBuilder.control('')]),
-  });
+
+  // Configuration object for the form
+  formConfig = {
+    firstName: { type: 'text', validators: [Validators.required] },
+    lastName: { type: 'text', validators: [] },
+    address: {
+      group: {
+        street: { type: 'text', validators: [] },
+        city: { type: 'text', validators: [] },
+        state: { type: 'text', validators: [] },
+        zip: { type: 'text', validators: [] },
+      },
+    },
+    aliases: { array: { type: 'text', validators: [] } },
+  };
+
+  profileForm = this.createForm(this.formConfig);
+
+  // Dynamically create the form structure
+  private createForm(config: any): FormGroup {
+    const group: any = {};
+    for (const key in config) {
+      if (config[key].group) {
+        group[key] = this.createForm(config[key].group);
+      } else if (config[key].array) {
+        group[key] = this.formBuilder.array([
+          this.createControl(config[key].array),
+        ]);
+      } else {
+        group[key] = this.createControl(config[key]);
+      }
+    }
+    return this.formBuilder.group(group);
+  }
+
+  private createControl(config: any): FormControl {
+    return this.formBuilder.control('', config.validators || []);
+  }
 
   get aliases() {
     return this.profileForm.get('aliases') as FormArray;
   }
+
   addAlias() {
-    this.aliases.push(this.formBuilder.control(''));
+    this.aliases.push(this.createControl(this.formConfig.aliases.array));
   }
 }
